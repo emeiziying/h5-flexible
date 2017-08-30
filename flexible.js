908fc99fddb693f7bbacaeff12ca8bb32f2564ff
Quan <Quan@Quan-MBP.local>
2017-08-30 15:17:59 +0800

    update

--- flexible.js
+++ flexible.js
@@ -1,53 +1,63 @@
-//author:caibaojian
-//website:http://caibaojian.com
-//weibo:http:weibo.com/kujian
-//兼容UC竖屏转横屏出现的BUG
-//自定义设计稿的宽度：designWidth
-//最大宽度:maxWidth
-//这段js的最后面有两个参数记得要设置，一个为设计稿实际宽度，一个为制作稿最大宽度，例如设计稿为750，最大宽度为750，则为(750,750)
-;(function (designWidth, maxWidth) {
-	var doc      = document,
-	    win      = window,
-	    docEl    = doc.documentElement,
-	    remStyle = document.createElement("style"),
-	    tid;
+(function (designWidth, designHeight) {
+	designWidth  = designWidth || 640;
+	designHeight = designHeight || 1008;
+
+	var docEl    = document.documentElement;
+	var remStyle = document.createElement("style");
+	var dpr      = window.devicePixelRatio || 1;
+	var tid      = null;
 
 	function refreshRem() {
-		var width = docEl.getBoundingClientRect().width;
-		maxWidth  = maxWidth || 640;
-		width > maxWidth && (width = maxWidth);
-		var rem            = width * 100 / designWidth;
+		var width  = docEl.getBoundingClientRect().width;
+		var height = docEl.getBoundingClientRect().height;
+
+		var rem = width * 100 / designWidth;
+
 		remStyle.innerHTML = 'html{font-size:' + rem + 'px;}';
 	}
 
 	if (docEl.firstElementChild) {
 		docEl.firstElementChild.appendChild(remStyle);
 	} else {
-		var wrap = doc.createElement("div");
+		var wrap = document.createElement("div");
 		wrap.appendChild(remStyle);
-		doc.write(wrap.innerHTML);
+		document.write(wrap.innerHTML);
 		wrap = null;
 	}
+
 	//要等 wiewport 设置好后才能执行 refreshRem，不然 refreshRem 会执行2次；
 	refreshRem();
 
-	win.addEventListener("resize", function () {
+	window.addEventListener("resize", function () {
 		clearTimeout(tid); //防止执行两次
 		tid = setTimeout(refreshRem, 300);
 	}, false);
 
-	win.addEventListener("pageshow", function (e) {
+	window.addEventListener("pageshow", function (e) {
 		if (e.persisted) { // 浏览器后退的时候重新计算
 			clearTimeout(tid);
 			tid = setTimeout(refreshRem, 300);
 		}
 	}, false);
 
-	if (doc.readyState === "complete") {
-		doc.body.style.fontSize = "16px";
+	if (document.readyState === "complete") {
+		document.body.style.fontSize = "16px";
 	} else {
-		doc.addEventListener("DOMContentLoaded", function (e) {
-			doc.body.style.fontSize = "16px";
+		document.addEventListener("DOMContentLoaded", function (e) {
+			document.body.style.fontSize = "16px";
 		}, false);
 	}
-})(640, 640);
+
+	// detect 0.5px supports
+	if (dpr >= 2) {
+		var fakeBody             = document.createElement('body');
+		var testElement          = document.createElement('div');
+		testElement.style.border = '.5px solid transparent';
+		fakeBody.appendChild(testElement);
+		docEl.appendChild(fakeBody);
+		if (testElement.offsetHeight === 1) {
+			docEl.classList.add('hairlines')
+		}
+		docEl.removeChild(fakeBody);
+	}
+}(640, 1008));
